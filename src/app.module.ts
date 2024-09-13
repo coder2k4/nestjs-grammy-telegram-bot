5fb7dd2d491b745c9c19960443f748aadfb78892
G.A.S. Standalone DuramboRUS <coder2k@mail.ru>
2024-09-13 09:40:14 +0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
-
+import { TelegrambotModule } from './telegrambot/telegrambot.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TelegrambotModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Это делает ConfigModule глобальным модулем, чтобы не нужно было импортировать его в каждый модуль
+    }),
+  ],
 })
 export class AppModule {}
